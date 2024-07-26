6e41e31b5b51002689ad4a17f4044c070fa545c9
Eui Lee <lechan1317@gmail.com>
2024-07-26 17:08:31 +0900

    feat: arrow-up

--- dream_coding/portfolio/src/main.js
+++ dream_coding/portfolio/src/main.js
@@ -16,3 +16,13 @@ const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 })
+
+// arrow up button
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+})
