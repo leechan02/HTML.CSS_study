b2f80f8c41243a7272bcab2ebb5fd2c4363916f6
Eui Lee <lechan1317@gmail.com>
2024-07-26 20:50:37 +0900

    feat: scroll section

--- dream_coding/portfolio/src/active_menu.js
+++ dream_coding/portfolio/src/active_menu.js
@@ -1,20 +1,23 @@
-"use strict";
-
 const sectionIds = [
-  "#home",
-  "#about",
-  "#skills",
-  "work",
-  "#testimonials",
-  "#contact",
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonial',
+  '#contact',
 ];
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
   document.querySelector(`[href="${id}"]`)
 );
-const visibleSections = sectionIds.map(() => false);
 
-const options = {};
+const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
+
+const options = {
+  rootMargin: '-20% 0px 0px 0px',
+  threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
 
@@ -26,15 +29,24 @@ function observerCallback(entries) {
     selectLastOne =
       index === sectionIds.length - 1 &&
       entry.isIntersecting &&
-      entry.intersectionRatio >= 0.5;
+      entry.intersectionRatio >= 0.95;
   });
 
   const navIndex = selectLastOne
     ? sectionIds.length - 1
     : findFirstIntersecting(visibleSections);
+  selectNavItem(navIndex);
 }
 
-function findFirstIntersecting(sections) {
-  const index = sections.indexOf(true);
+function findFirstIntersecting(intersections) {
+  const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove('active');
+  activeNavItem = navItem;
+  activeNavItem.classList.add('active');
 }
